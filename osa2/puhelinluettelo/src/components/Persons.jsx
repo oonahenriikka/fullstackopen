a8c10943b9a2fe25683cef2aeae0bf7edac93471
oonahenriikka <144310036+oonahenriikka@users.noreply.github.com>
2024-12-20 14:20:11 +0200

    Add files via upload

---
+++ osa2/puhelinluettelo/src/components/Persons.jsx
@@ -0,0 +1,14 @@
+const Persons = ({ persons, deletePerson }) => {
+  return (
+    <ul>
+      {persons.map(person => 
+        <li key={person.id}>
+          {person.name} {person.number}
+          <button onClick={() => deletePerson(person.id)}>delete</button>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Persons;
