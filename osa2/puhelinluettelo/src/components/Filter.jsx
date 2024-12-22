4f5d6d504b7754d6e7fb16ff067c447524b89ff5
oonahenriikka <144310036+oonahenriikka@users.noreply.github.com>
2024-12-22 11:16:56 +0200

    Delete osa2/puhelinluettelo directory

--- osa2/puhelinluettelo/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ searchTerm, handleSearchChange }) => {
-  return (
-    <div>
-      filter shown with: <input value={searchTerm} onChange={handleSearchChange} />
-    </div>
-  )
-}
-
-export default Filter
